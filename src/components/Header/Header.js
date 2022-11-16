f7a9e56e189c07f2b341cc5c4ca8afc32abefc31
kushagra <kushagratripathi7@gmail.com>
2022-11-16 20:05:51 +0530

    learn: how to select specific element and check the condition

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,16 @@
-import React from 'react'
-import "./Header.css"
+import React from "react";
+import "./Header.css";
 
-export default function Header({
-    title
-}) {
-    return <h1 className="header">{title}</h1>
+export default function Header({ title }) {
+  return (
+    <>
+      {/* <h1 title="Header" className="header">
+        {title}
+      </h1> */}
+      <h1 title="Header" className="header" data-testId="header-1">
+        My Header
+      </h1>
+      <h1 className="header">Kush</h1>
+    </>
+  );
 }
---
+++ src/components/Header/__test__/Header.test.js
@@ -0,0 +1,52 @@
+import { render, screen } from "@testing-library/react";
+import Header from "../Header";
+
+// it("Should render same text passed into the title prop", async () => {
+//   render(<Header title="My Header" />);
+//   const headingElement = screen.getByText(/my header/i);
+//   expect(headingElement).toBeInTheDocument();
+// });
+
+// In case we have multiple element and want to select specific heading then we will specify it in the object
+
+// it("Should render the heading which contains My header Text", async () => {
+//   render(<Header title="My Header" />);
+//   const headingElement = screen.getByRole("heading", { name: "My Header" });
+//   expect(headingElement).toBeInTheDocument();
+// });
+
+it("Should render the heading element", async () => {
+  render(<Header title="My Header" />);
+  const headingElement = screen.getByTitle("Header");
+  expect(headingElement).toBeInTheDocument();
+});
+
+it("Should render the heading element of the specific ID", async () => {
+  render(<Header title="My Header" />);
+  const headingElement = screen.getByTestId("header-1");
+  expect(headingElement).toBeInTheDocument();
+});
+
+// FIND BY ATTRIBUTE
+
+it("Should render same text passed into the title prop", async () => {
+  render(<Header title="My Header" />);
+  const headingElement = await screen.findByText(/my header/i);
+  expect(headingElement).toBeInTheDocument();
+});
+
+// QUERYBY -> not give error if the eleemnt is not present
+
+it("Should render if present and ignore if not", async () => {
+  render(<Header title="My Header" />);
+  const headingElement = screen.queryByText(/asdkskjfhdskjfdskjf/i);
+  expect(headingElement).not.toBeInTheDocument();
+});
+
+// GET ALL BY ROles
+
+it("Should render all the headings", async () => {
+  render(<Header title="My Header" />);
+  const headingElement = screen.queryByText(/asdkskjfhdskjfdskjf/i);
+  expect(headingElement.length).toBe(2);
+});
